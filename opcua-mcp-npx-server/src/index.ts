216a983b181cf4c91ba92c79ff0877375a331212
Nick Brachet <nbrachet@gmail.com>
2026-04-27 15:54:09 -0400

    keep versioning in-sync

--- opcua-mcp-npx-server/src/index.ts
+++ opcua-mcp-npx-server/src/index.ts
@@ -38,7 +38,7 @@ class OPCUAMCPServer {
     this.server = new Server(
       {
         name: "opcua-mcp-npx-server",
-        version: "0.1.0",
+        version: "0.1.2",
       },
       {
         capabilities: {
